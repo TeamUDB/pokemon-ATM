823591773d01624eb5950264606caeccb1ba50db
Victor Hugo Cornejo Calderon <rex2002xp@gmail.com>
2023-12-02 17:29:21 -0600

    Se completo la consulta de Saldos.

--- src/pages/balance/BalanceStep02.tsx
+++ src/pages/balance/BalanceStep02.tsx
@@ -2,31 +2,47 @@ import InfoSection from "../../components/board/InfoSection.tsx";
 import { useEffect, useState } from "react";
 import Accounts from "../../shared/accounts/Accounts.tsx";
 import { useNavigate } from "react-router-dom";
+import { useBalance } from "../../hooks/useBalance.tsx";
 
 const BalanceStep02 = () => {
-  const [ exit, setExit ] = useState(false);
-  const [ , setAccountSelected ] = useState<string>("");
-
+  const [accountSelected, setAccountSelected] =
+    useState<string>(localStorage.getItem('accountSelected') || "");
+  const [exit, setExit] = useState(false);
   const navigate = useNavigate();
+  const {data, isLoading} = useBalance(2542, accountSelected);
+
+  useEffect(() => {
+    console.log(`Estatus => ${ data}`);
+  }, [data]);
 
   useEffect(() => {
     if (exit) {
+
       localStorage.removeItem('accountSelected');
       navigate('/main');
     }
     return () => {
       setExit(false);
     }
-  }, [ exit ]);
+  }, [exit]);
 
   return (
     <div className="h-screen flex justify-center items-center">
-      <div className={ "w-full h-[100vh] pt-10 board" }>
-        <Accounts accounts={ [] } setBack={ setExit } setAccountSelected={ setAccountSelected }/>
+      <div className={"w-full h-[100vh] pt-10 board"}>
+        <Accounts accounts={[]} setBack={setExit} setAccountSelected={setAccountSelected}/>
         <InfoSection>
-          <div className={ "flex flex-col items-center w-full" }>
-            <h2 className={ "font-light text-4xl" }>Su Saldo es: $ 1000.00</h2>
-          </div>
+          {
+            isLoading ? (
+              <div className={"flex flex-col items-center w-full"}>
+                <h2 className={"font-light text-4xl"}>Consultado...</h2>
+              </div>
+            ) : (
+              <div className={"flex flex-col items-center w-full"}>
+                <h2 className={"font-bold text-4xl"}>Cuenta : {accountSelected}</h2>
+                <h2 className={"font-light text-4xl"}>Su Saldo es: $ {data}</h2>
+              </div>
+            )
+          }
         </InfoSection>
       </div>
     </div>
--- src/services/accouts.services.tsx
+++ src/services/accouts.services.tsx
@@ -7,29 +7,36 @@ export type AccountInfo = {
   currency: string;
 }
 
+const accounts: AccountInfo[] = [
+  {
+    id: 1,
+    customerId: 2542,
+    name: "Pedro Perez",
+    accountNumber: "008653321",
+    balance: 2000,
+    currency: "USD"
+  },
+  {
+    id: 2,
+    customerId: 2542,
+    name: "Pedro Perez",
+    accountNumber: "008653324",
+    balance: 500,
+    currency: "USD"
+  },
+];
+
 
 export const AccountsServices = (customerId: number) => {
-  const accounts: AccountInfo[] = [
-    {
-      id: 1,
-      customerId: 2542,
-      name: "Pedro Perez",
-      accountNumber: "008653321",
-      balance: 2000,
-      currency: "USD"
-    },
-    {
-      id: 2,
-      customerId: 2542,
-      name: "Pedro Perez",
-      accountNumber: "008653324",
-      balance: 500,
-      currency: "USD"
-    },
-  ];
   return new Promise<AccountInfo[]>((resolve) => {
     setTimeout(() => {
       resolve(accounts.filter(account => account.customerId === customerId));
     }, 500);
+  });
+}
+
+export const getBalance = (customerId: number, accountNumber: string) => {
+  return new Promise<number>((resolve) => {
+      resolve(accounts.find(account => account.customerId === customerId && account.accountNumber === accountNumber)?.balance || 0);
   });
 }
